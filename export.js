92573c337009af3326809bde445d2e40353216f8
lachero-work <asaf@gwumkt.com>
2021-04-12 15:32:10 +0300

    fixed , bug

--- export.js
+++ export.js
@@ -2,7 +2,8 @@ const fs = require('fs');
 const en = require('./en.json');
 const { getValues } = require('./utils');
 
-const file = ['EN', ...getValues(en)];
+const file = ['"EN"', ...getValues(en)];
+console.log(file);
 
 fs.writeFileSync('translations.csv', file.join('\n'));
-console.log('Done! ./translations.csv')
+console.log('Done! ./translations.csv');
--- utils.js
+++ utils.js
@@ -1,17 +1,17 @@
 const list = [];
 
 const pushToList = data => {
-    Object.values(data).forEach(v => {
-        if(typeof v === 'object') return pushToList(v)
-        list.push(v);
-    });
+	Object.values(data).forEach(v => {
+		if (typeof v === 'object') return pushToList(v);
+		list.push(v);
+	});
 };
 
 const getValues = object => {
-    pushToList(Object.values(object));
-    return list;
+	pushToList(Object.values(object));
+	return list.map(i => `"${i}"`);
 };
 
 module.exports = {
-    getValues,
-}
+	getValues,
+};
