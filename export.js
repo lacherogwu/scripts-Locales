07e1d6c85a3b48e0e79e9029abafdc0619228b42
lachero-work <asaf@gwumkt.com>
2021-04-12 15:32:39 +0300

    remove console.log

--- export.js
+++ export.js
@@ -3,7 +3,6 @@ const en = require('./en.json');
 const { getValues } = require('./utils');
 
 const file = ['"EN"', ...getValues(en)];
-console.log(file);
 
 fs.writeFileSync('translations.csv', file.join('\n'));
 console.log('Done! ./translations.csv');
